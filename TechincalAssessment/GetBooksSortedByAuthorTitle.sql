b8c9d79292f1a2f538029f93926154ea67381b15
vivitha.palanisamy <vivitha.palanisamy@outlook.com>
2023-12-04 11:09:18 +0530

    Readme and StoredProcedure file inserted

---
+++ TechincalAssessment/GetBooksSortedByAuthorTitle.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE GetBooksSortedByAuthorTitle
+AS
+BEGIN
+    SELECT 
+        b.Id,
+        b.Title,
+        b.Price,
+		b.AuthorId,
+        b.PublisherId,
+        a.LastName AS AuthorLastName,
+        a.FirstName AS AuthorFirstName,
+        p.Name AS Publisher
+    FROM Books b
+    INNER JOIN Authors a ON b.AuthorId = a.AuthorId
+    INNER JOIN Publishers p ON b.PublisherId = p.PublisherId
+    ORDER BY 
+        a.LastName, 
+        a.FirstName, 
+        b.Title;
+END;
---
+++ TechincalAssessment/GetBooksSortedByPublisherAuthorTitle.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE GetBooksSortedByPublisherAuthorTitle
+AS
+BEGIN
+    SELECT 
+        b.Id,
+        b.Title,
+        b.Price,
+		b.AuthorId,
+        b.PublisherId,
+        p.Name AS PublisherName,
+        a.LastName AS AuthorLastName,
+        a.FirstName AS AuthorFirstName
+    FROM Books b
+    INNER JOIN Publishers p ON b.PublisherId = p.PublisherId
+    INNER JOIN Authors a ON b.AuthorId = a.AuthorId
+    ORDER BY 
+        p.Name, 
+        a.LastName, 
+        a.FirstName, 
+        b.Title;
+END;
